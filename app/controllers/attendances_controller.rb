78176dd57a55a0bbbb86c800c43a71b0a91ad042
mnx <mmxnw150bnec98@yahoo.co.jp>
2022-02-11 17:08:19 +0000

    Update Gemfile for Heroku

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,6 @@
 class AttendancesController < ApplicationController
+  include AttendancesHelper
+
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
@@ -7,7 +9,6 @@ class AttendancesController < ApplicationController
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
-    @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
     if @attendance.started_at.nil?
@@ -31,18 +32,23 @@ class AttendancesController < ApplicationController
   
   def update_one_month
     ActiveRecord::Base.transaction do # トランザクションを開始します。
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      if attendances_invalid?
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
+        end
+        flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+        flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
       end
     end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-  
+
   private
     # 1ヶ月分の勤怠情報を扱います。
     def attendances_params
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,29 @@ module AttendancesHelper
   def working_times(start, finish)
      format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  def format_hour(time)
+    format('%.d',((time.hour)))
+  end
+  
+  def format_min(time)
+    format("%.2d",((time.strftime('%M').to_i / 15).round) * 15)
+  end
+  
+     # 不正な値があるか確認する
+  def attendances_invalid?
+    attendances = true
+    attendances_params.each do |id, item|
+      if item[:started_at].blank? && item[:finished_at].blank?
+        next
+      elsif item[:started_at].blank? || item[:finished_at].blank?
+        attendances = false
+        break
+      elsif item[:started_at] > item[:finished_at]
+        attendances = false
+        break
+      end
+    end
+    return attendances
+  end
 end
