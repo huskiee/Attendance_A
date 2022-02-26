f022e32b80c6f6ef4fd998f871cca054bb72cce9
EC2 Default User <ec2-user@ip-172-31-42-133.ap-northeast-1.compute.internal>
2022-02-26 13:36:00 +0000

    Finish top-page for Attendance_A

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20220129123850) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-01-29 23:00:00"
-    t.datetime "work_time", default: "2022-01-29 22:30:00"
+    t.datetime "basic_time", default: "2022-02-25 23:00:00"
+    t.datetime "work_time", default: "2022-02-25 22:45:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
